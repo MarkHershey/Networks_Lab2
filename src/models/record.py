381e834ee50bec60fecd74690cecbb8c8f21be98
Mark Huang <hhzeos@gmail.com>
2021-09-26 16:07:26 +0800

    Update record endpoint

--- src/models/record.py
+++ src/models/record.py
@@ -2,17 +2,20 @@ from datetime import datetime
 from typing import List, Optional, Tuple
 
 from markkk.logger import logger
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+
+from ..functional import uid_gen
 
 
 class Record(BaseModel):
-    uid: str = None  # imply time of record creation
-    date_time: datetime  # time of purchase
+    uid: Optional[str] = None  # imply time of record creation
+    date_time: Optional[datetime] = None  # time of purchase
     amount: float  # debit amount or credit amount
-    merchant: str = None  # seller
-    category: str = None
-    subcategory: str = None
-    location: str = None  # geographic location of the transaction
+    amount_abs: Optional[float] = None  # absolute amount >= 0
+    merchant: Optional[str] = None  # seller
+    category: Optional[str] = None
+    subcategory: Optional[str] = None
+    location: Optional[str] = None  # geographic location of the transaction
     link: Optional[str] = None  # online product link
     tags: List[str] = []  # user-defined tags
     reference: Optional[str] = None  # reference number from the receipt / bank record
@@ -20,3 +23,15 @@ class Record(BaseModel):
     star_flag: bool = False  # init to false
     confirmed_flag: bool = False  # init to false if record is auto generated
     exclude_flag: bool = False  # init to false
+
+    @validator("uid", pre=True, always=True)
+    def default_uid(cls, v):
+        return v or uid_gen("R")
+
+    @validator("date_time", pre=True, always=True)
+    def default_date_time(cls, v):
+        return v or datetime.now()
+
+    @validator("amount_abs", pre=True, always=True)
+    def absolute_amount(cls, v, *, values, **kwargs):
+        return v or abs(values["amount"])
--- src/routes/record.py
+++ src/routes/record.py
@@ -1,5 +1,8 @@
 # purchase records
 
+from datetime import datetime
+from typing import List, Optional, Tuple
+
 from fastapi import APIRouter, Depends, HTTPException
 from markkk.logger import logger
 from pymongo import ReturnDocument
@@ -15,7 +18,74 @@ router = APIRouter(prefix="/api/records", tags=["Purchase Record"])
 auth_handler = AuthHandler()
 
 
-@router.post("/new", status_code=201)
+@router.get("/", response_model=List[Record])
+async def get_records(
+    start_time: Optional[datetime] = None,
+    end_time: Optional[datetime] = None,
+    tag: Optional[str] = None,
+    offset: int = 0,
+    count: int = -1,
+    sort_by: str = "transaction time",
+    reverse: bool = False,
+    username=Depends(auth_handler.auth_wrapper),
+):
+    # TODO: user validation
+    logger.debug(f"User({username}) fetching records")
+
+    # record_dict = dict(new_record.dict())
+
+    sort_by_mapping = {
+        "record time": "uid",
+        "transaction time": "date_time",
+        "amount": "amount",
+        "absolute amount": "amount",
+        "merchant": "merchant",
+        "category": "category",
+        "subcategory": "subcategory",
+    }
+
+    sort_key: str = sort_by_mapping.get(sort_by, "date_time")
+    offset = 0 if not isinstance(offset, int) or offset < 0 else offset
+    if count == 0:
+        return []
+
+    try:
+        user_data_dict: dict = user_data_collection.find_one({"username": username})
+        clean_dict(user_data_dict)
+    except Exception as e:
+        logger.error(MSG.DB_QUERY_ERROR)
+        logger.error(e)
+        raise HTTPException(status_code=500, detail=MSG.DB_QUERY_ERROR)
+
+    if not user_data_dict:
+        raise HTTPException(status_code=404, detail=MSG.DB_QUERY_ERROR)
+
+    user_records: List[dict] = user_data_dict.get("records")
+    q_results: List[dict] = []
+
+    if start_time or end_time or tag:
+        start_time = datetime(1970, 1, 1) if not start_time else start_time
+        end_time = datetime.now() if not end_time else end_time
+        filter_tag = False if tag is None else True
+
+        for r in user_records:
+            _transaction_time: datetime = r.get("date_time")
+            if start_time <= _transaction_time <= end_time:
+                if filter_tag and tag in r.get("tags", []):
+                    q_results.append(r)
+
+    q_results = list(sorted(q_results, key=lambda x: x[sort_key], reverse=reverse))
+
+    if offset and offset < len(q_results):
+        q_results = q_results[offset:]
+
+    if count > 0:
+        q_results = q_results[:count]
+
+    return q_results
+
+
+@router.post("/", status_code=201)
 async def create_new_record(
     new_record: Record, username=Depends(auth_handler.auth_wrapper)
 ):
